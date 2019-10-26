8dfccf9f8fa0d9229e8feafd89c3c6302b1f41b7
Juan Carlos Galan Hernandez <juan.galan@gmail.com>
2019-10-26 14:00:56 -0500

    Add simple 2d movement (no collision)

---
+++ Assets/Source/Components/AxisXyValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source.Components
+{
+    [CreateAssetMenu]
+    public class AxisXyValue: ScriptableObject
+    {
+        public float x;
+        public float y;
+    }
+}
---
+++ Assets/Source/Components/SpeedValue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source.Components
+{
+    [CreateAssetMenu]
+    public class SpeedValue: ScriptableObject
+    {
+        public float value = 1;
+    }
+}
